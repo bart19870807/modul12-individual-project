f77590b56f3f080722b679bf244050d8cec70836
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-11-01 15:19:09 +0000

    added settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,4 +1,9 @@
-// db: {
-//     url: '//localhost:3131',
-//     products: 'products'
-// }  
+// eslint-disable-next-line no-unused-vars
+const settings = {
+  db: {
+    url: '//localhost:3131',
+    products: 'products'
+  },
+};
+
+ 
