7013fb318684d6f0ac436a461833cc316c726eca
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-11-24 03:53:36 +0000

    add and remove active class

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,10 @@
 // import { settings } from './settings.js';
 // {
-//   'use strict';
+'use strict';
+
+// const { data } = require("autoprefixer");
+// const { create } = require("browser-sync");
+
     
 //   const app = {
 //     initData: function() {
@@ -27,14 +31,51 @@
 //   app.init();
 // }
 
-const url = 'http://localhost:3131/products';
+// const urlWithBadDomain = 'http://localhost:3131/products';
 
-fetch(url)
-  .then(function(rawResponse){
-    return rawResponse.json();
+// fetch(url)
+//   .then(function(rawResponse){
+//     return rawResponse.json();
+//   })
+//   .then(function(parsedResponse){
+//     console.log(parsedResponse);
+//   });
+
+// fetch(urlWithBadDomain)
+//   .then(rawResponse => {
+//     if (rawResponse.status >= 200 && rawResponse.status < 300) {
+//       return rawResponse.json();
+//     } else {
+//       return Promise.reject(rawResponse.status + ' ' + rawResponse.statusText);
+//     }
+//   })
+//   .then(parsedResponse => {
+//     console.log(parsedResponse);
+//   })
+//   .catch((error)=>{
+//     console.log('CONNECTION ERROR', error);
+//   });
+
+// console.log('hello');
+
+// function createHTML(productsData) {
+//   let rawTemplate = document.getElementById('productTamplet').innerHTML;
+//   let compiledTemplate = Handlebars.compile(rawTemplate);
+//   let ourGeneratedHTML = compiledTemplate(productsData);
+//   console.log(rawTemplate);
+// }
+
+const naviElement = document.querySelectorAll('#navi a');
+naviElement.forEach(function(element){
+  element.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const activeElement = document.querySelector('.page.active');
+    activeElement.classList.remove('active');
+
+    const href = element.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.classList.add('active');
   })
-  .then(function(parsedResponse){
-    console.log(parsedResponse);
-  });
-
-console.log('hello');
+})
